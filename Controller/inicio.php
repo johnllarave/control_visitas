70635abcc503419f3cd513b35e4d927af9cd8f7b
Jose Sanchez <josesanchezgaona@gmail.com>
2020-05-28 17:56:46 -0500

    Segunda actualizacion: Inclusion de librerias en archivo: inicio.php

--- Controller/inicio.php
+++ Controller/inicio.php
@@ -6,3 +6,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
         <title>Inicio</title>
+
+        <!--<link rel="icon" href="img/favicon.ico" type="image/x-icon">-->
+        <link href="css/bootstrap.css" rel="stylesheet">   
+        <link href="css/metisMenu.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/sb-admin-2.css" rel="stylesheet">
+        <link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+        <link href="css/plugins/toastr/toastr.min.css" rel="stylesheet">
+        <head>
+
