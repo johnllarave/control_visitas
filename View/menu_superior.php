0fe8d3b48ec858d7bac83f053c4f62be173bff45
John Alexander Llarave <jallarave@gmail.com>
2020-06-15 12:33:17 -0500

    Nuevas opciones
    Opciones de usuario

--- View/menu_superior.php
+++ View/menu_superior.php
@@ -15,3 +15,19 @@ if ($_SESSION['nombre'] == "") {
     </button>
     <a class="navbar-brand" href="inicio.php">GESTION DE VISITAS</a>
 </div>
+<ul class="nav navbar-top-links navbar-right">
+    <li class="dropdown">
+        <h4 class="dropdown-toggle"> <?php echo $_SESSION['nombre'] . " " . $_SESSION['apellido']; ?></h4>
+    </li>
+    <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+            <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+        </a>
+        <ul class="dropdown-menu dropdown-user">
+            <li><a href="actualiza_datos.php"><i class="fa fa-user fa-fw"></i> Actualizar datos</a></li>
+            <li><a href="actualiza_pass.php"><i class="fa fa-gear fa-fw"></i> Actualizar contraseña</a></li>
+            <li class="divider"></li>
+            <li><a href="destruir_sesion.php"><i class="fa fa-sign-out fa-fw"></i>Salir</a></li>
+        </ul>
+    </li>
+</ul>
