8878740da6bd179256086f8e5c142c60c14a8bc5
John Alexander Llarave <jallarave@gmail.com>
2020-06-15 12:32:46 -0500

    DIV principal
    Se crea el div que contiene el titulo del proyecto

--- View/menu_superior.php
+++ View/menu_superior.php
@@ -6,3 +6,12 @@ if ($_SESSION['nombre'] == "") {
     header("location:../index.php");
 }
 ?>
+<div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="inicio.php">GESTION DE VISITAS</a>
+</div>
