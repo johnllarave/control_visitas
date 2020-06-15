710f8ddf30d1a7af218c2062b169f6a9c8a85cfb
John Alexander Llarave <jallarave@gmail.com>
2020-06-15 12:35:29 -0500

    opciones
    Se crean las ociones para la creación de los nuevos modulos

--- View/menu_lateral.php
+++ View/menu_lateral.php
@@ -4,5 +4,15 @@
         <a href="inicio.php"><i class="fa fa-home"></i>Inicio</a>
     </li>
 
-
+    <li>
+        <a href="clientes.php"><i class="fa fa-comments"></i> Clientes</a>
+    </li>
+    
+    <li>
+        <a href="form_visitas.php"><i class="fa fa-file-text-o"></i> Visitas</a>
+    </li>
+    
+    <li>
+        <a href="visitas.php"><i class="fa fa-file-excel-o"></i> Reportes</a>
+    </li>
 </ul>
