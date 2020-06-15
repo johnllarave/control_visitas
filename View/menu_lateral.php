5b39166b349ae3897cd0190fda3a3ab0deef3d7f
John Alexander Llarave <jallarave@gmail.com>
2020-06-15 12:36:34 -0500

    Update menu_lateral.php
    Se configuran las opciones para ingresar al menu segun el rol del usuario logeado

--- View/menu_lateral.php
+++ View/menu_lateral.php
@@ -3,7 +3,23 @@
     <li>
         <a href="inicio.php"><i class="fa fa-home"></i>Inicio</a>
     </li>
+    <?php
+        if ($_SESSION['id_rol'] == '1') {
+    ?>
+            <li>
+                <a href="usuarios.php"><i class="fa fa-user-plus"></i> Usuarios</a>
+            </li>
+    <?php
+        }
 
+        if ($_SESSION['id_rol'] == '2') {
+    ?>
+            <li>
+                <a href="usuarios.php"><i class="fa fa-user-plus"></i> Usuarios</a>
+            </li>
+    <?php
+        }
+    ?>
     <li>
         <a href="clientes.php"><i class="fa fa-comments"></i> Clientes</a>
     </li>
